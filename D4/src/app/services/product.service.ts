5c2cefe9e744ef2c35ba942d8440ea7561059958
mohab <mohabsoliman50@gmail.com>
2024-08-21 00:56:00 +0300

    D4

---
+++ D4/src/app/services/product.service.ts
@@ -0,0 +1,36 @@
+import { IProduct } from './../models/iproduct';
+import { Injectable } from '@angular/core';
+
+import { productsList } from '../models/products';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  products: IProduct[];
+  constructor() {
+    this.products = productsList;
+  }
+
+  getAllProducts(): IProduct[] {
+    return this.products;
+  }
+
+  getProductById(productId: any): IProduct | undefined {
+    return this.products.find((product) => product.id == productId);
+  }
+  addProduct(product: any) {
+    return this.products.push(product);
+  }
+  deleteProduct(productId: any) {
+    this.products = this.products.filter((product) => product.id != productId);
+
+    return this.products;
+  }
+  updateProduct(product: any) {
+    const index = this.products.findIndex(p => p.id === product.id);
+    if (index !== -1) {
+      this.products[index] = product;
+    }
+  }
+}
